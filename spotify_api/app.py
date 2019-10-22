23acf1344039757290e2184800492a719191bf22
matthew-sessions <matthew-sessions@users.noreply.github.com>
2019-10-21 19:28:41 -0700

    Added api key to urls

--- spotify_api/app.py
+++ spotify_api/app.py
@@ -6,6 +6,8 @@ from work_data import *
 
 load_dotenv()
 
+API_KEY = config("API_KEY")
+
 app = Flask(__name__)
 
 
@@ -13,37 +15,42 @@ app = Flask(__name__)
 def home():
     return(render_template('home.html'))
 
-@app.route('/songs/<value>')
-def song_search(value):
-    songs = get_songs(value)
-    return(jsonify(songs))
+@app.route('/songs/<key>/<value>')
+def song_search(key,value):
+    if key == API_KEY:
+        songs = get_songs(value)
+        return(jsonify(songs))
 
-@app.route('/features/<value>')
-def song_features(value):
-    features = get_features(value)
-    return(jsonify(features))
+@app.route('/features/<key>/<value>')
+def song_features(key,value):
+    if key == API_KEY:
+        features = get_features(value)
+        return(jsonify(features))
 
-@app.route('/recs/<value>')
-def recommendations(value):
-    recs = Five_recs(value)
-    all_recs = recs.get_100()
-    feature_df = recs.get_all_features(all_recs)
-    target_song = recs.get_all_features(value)
-    five_recs_df = recs.top_recs(feature_df, target_song)
-    five_recs = recs.feedback(five_recs_df)
+@app.route('/recs/<key>/<value>')
+def recommendations(key,value):
+    if key == API_KEY:
+        recs = Five_recs(value)
+        all_recs = recs.get_100()
+        feature_df = recs.get_all_features(all_recs)
+        target_song = recs.get_all_features(value)
+        five_recs_df = recs.top_recs(feature_df, target_song)
+        five_recs = recs.feedback(five_recs_df)
 
-    return(jsonify(five_recs))
+        return(jsonify(five_recs))
 
 
-@app.route('/embed/<value>')
-def embed(value):
-    recs = Five_recs(value)
-    all_recs = recs.get_100()
-    feature_df = recs.get_all_features(all_recs)
-    target_song = recs.get_all_features(value)
-    five_recs_df = recs.top_recs(feature_df, target_song)
-    five_recs = recs.feedback(five_recs_df)
-    return(render_template('embed.html', five_recs=five_recs))
+@app.route('/embed/<key>/<value>')
+def embed(key,value):
+
+    if key == API_KEY:
+        recs = Five_recs(value)
+        all_recs = recs.get_100()
+        feature_df = recs.get_all_features(all_recs)
+        target_song = recs.get_all_features(value)
+        five_recs_df = recs.top_recs(feature_df, target_song)
+        five_recs = recs.feedback(five_recs_df)
+        return(render_template('embed.html', five_recs=five_recs))
 
 if __name__ == '__main__':
     app.run()
