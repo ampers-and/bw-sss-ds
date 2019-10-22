e5438070dd202aa68a4fc6116ea7a7a4e5384a68
AHartNtkn <AHartNtkn@users.noreply.github.com>
2019-10-21 18:44:45 -0700

    Improve style

--- spotify_api/app.py
+++ spotify_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect,jsonify
+from flask import Flask, render_template, request, url_for, redirect, jsonify
 from decouple import config
 from dotenv import load_dotenv
 from work_data import *
@@ -9,19 +9,23 @@ load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return(render_template('home.html'))
+
 
 @app.route('/songs/<value>')
 def song_search(value):
     songs = Rec_Helper().get_songs(value)
     return(jsonify(songs))
 
+
 @app.route('/features/<value>')
 def song_features(value):
     features = Rec_Helper().get_features(value)
     return(jsonify(features))
+
 
 @app.route('/recs/<value>')
 def recommendations(value):
@@ -29,11 +33,13 @@ def recommendations(value):
 
     return(jsonify(recs))
 
+
 @app.route('/embed/<value>')
 def embed(value):
     recs = Rec_Helper().feedback(value)
 
     return(render_template('embed.html', recs=recs))
+
 
 @app.route('/graph/<value>')
 def graph(value):
@@ -43,16 +49,20 @@ def graph(value):
     features = pd.concat([rec_helper.get_all_features([value]), recs])
 
     radar_chart = pygal.Radar()
-    radar_chart.title = 'Comparison of Recommendations for \"' + rec_helper.spotify.track(value)['name'] + '\"'
+    radar_chart.title = ('Comparison of Recommendations for \"' +
+                         rec_helper.spotify.track(value)['name'] + '\"')
     radar_chart.x_labels = list(features.keys())
 
-    for name, feature_vec in zip(features.id, features.drop('id', axis=1).values):
-        radar_chart.add(rec_helper.spotify.track(name)['name'],
+    for id, feature_vec in zip(features.id, features.drop('id', axis=1).values):
+        radar_chart.add(rec_helper.spotify.track(id)['name'],
                         rec_helper.spot_scaler.transform([feature_vec])[0])
 
     graph_data = radar_chart.render_data_uri()
 
-    return(render_template('radio.html', graph_data = graph_data, title='Song Feature Graph'))
+    return(render_template('radio.html',
+                           graph_data=graph_data,
+                           title='Song Feature Graph'))
+
 
 if __name__ == '__main__':
     app.run()
--- spotify_api/work_data.py
+++ spotify_api/work_data.py
@@ -13,6 +13,7 @@ from joblib import load
 CLIENT_ID_var = config("CLIENT_ID")
 CLIENT_SECRET_var = config("CLIENT_SECRET")
 
+
 class Rec_Helper():
     CLIENT_ID = CLIENT_ID_var
     CLIENT_SECRET = CLIENT_SECRET_var
@@ -22,19 +23,19 @@ class Rec_Helper():
     token = credentials.get_access_token()
 
     def __init__(self, k=5):
-      self.spotify = spotipy.Spotify(auth=self.token)
-      self.spot_scaler = load('SpotScaler.joblib')
-      self.k = k
-      
-    #get the ids of 100 recommendations from spotify
+        self.spotify = spotipy.Spotify(auth=self.token)
+        self.spot_scaler = load('SpotScaler.joblib')
+        self.k = k
+
+    # get the ids of 100 recommendations from spotify
     def get_100(self, target_id):
         res = self.spotify.recommendations(seed_tracks=[target_id], limit=100)
         ids = [i['id'] for i in res['tracks']]
         return(ids)
-    
+
     def get_features(self, list_id):
         res = self.spotify.audio_features(tracks=list_id)[0]
-        
+
         feat_dict = {'acousticness': res['acousticness'],
                      'danceability': res['danceability'],
                      'duration_ms': res['duration_ms'],
@@ -70,23 +71,23 @@ class Rec_Helper():
                                    'id'])
 
         for i in res:
-            feat_dict = {'acousticness':i['acousticness'],
-                         'danceability':i['danceability'],
-                         'duration_ms':i['duration_ms'],
-                         'energy':i['energy'],
-                         'instrumentalness':i['instrumentalness'],
-                         'key':i['key'],
-                         'liveness':i['liveness'],
-                         'loudness':i['loudness'],
-                         'mode':i['mode'],
-                         'speechiness':i['speechiness'],
-                         'tempo':i['tempo'],
-                         'time_signature':i['time_signature'],
-                         'valence':i['valence'],
-                         'id':i['id']}
-            df = df.append(feat_dict,ignore_index=True)
+            feat_dict = {'acousticness': i['acousticness'],
+                         'danceability': i['danceability'],
+                         'duration_ms': i['duration_ms'],
+                         'energy': i['energy'],
+                         'instrumentalness': i['instrumentalness'],
+                         'key': i['key'],
+                         'liveness': i['liveness'],
+                         'loudness': i['loudness'],
+                         'mode': i['mode'],
+                         'speechiness': i['speechiness'],
+                         'tempo': i['tempo'],
+                         'time_signature': i['time_signature'],
+                         'valence': i['valence'],
+                         'id': i['id']}
+            df = df.append(feat_dict, ignore_index=True)
         return(df)
-    
+
     def top_recs(self, target_id):
         df = self.get_all_features(self.get_100(target_id))
         song_df = self.get_all_features(target_id)
@@ -94,7 +95,8 @@ class Rec_Helper():
         df_scaled = self.spot_scaler.fit_transform(df.drop(['id'], axis=1))
         feats = self.spot_scaler.transform(song_df.drop(['id'], axis=1))
 
-        df['dist'] = list(map(lambda x: norm(x-np.array(feats)[0]), np.array(df_scaled)))
+        df['dist'] = list(map(lambda x: norm(x-np.array(feats)[0]),
+                              np.array(df_scaled)))
         top = df.sort_values(by='dist').iloc[0:self.k]
 
         return top.drop(['dist'], axis=1)
@@ -109,14 +111,15 @@ class Rec_Helper():
                               'small_image': tracks['album']['images'][2]['url'],
                               'artist': tracks['artists'][0]['name'],
                               'song_name': tracks['name'],
-                              'id': tracks['id'], 'uri':tracks['uri']})
+                              'id': tracks['id'],
+                              'uri': tracks['uri']})
         return(song_info)
 
     def get_songs(self, song, limit=7):
         songs = self.spotify.search(song, limit=limit, offset=0, type='track', market='US')
         tracks = []
         for i in songs['tracks']['items']:
-            tracks.append({'song_name':i['name'],
-                           'artist':i['artists'][0]['name'],
-                           'id':i['id']})
+            tracks.append({'song_name': i['name'],
+                           'artist': i['artists'][0]['name'],
+                           'id': i['id']})
         return(tracks)
