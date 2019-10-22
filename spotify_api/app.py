6299654f3540800fae48a68487fa0f9fbfd230eb
AHartNtkn <AHartNtkn@users.noreply.github.com>
2019-10-22 14:09:58 -0700

    Factor work_data methods out into functions

--- spotify_api/app.py
+++ spotify_api/app.py
@@ -22,21 +22,21 @@ def home():
 @app.route('/songs/<key>/<value>')
 def song_search(key, value):
     if key == API_KEY:
-        songs = Rec_Helper().get_songs(value)
+        songs = get_songs(value)
         return(jsonify(songs))
 
 
 @app.route('/features/<key>/<value>')
 def song_features(key, value):
     if key == API_KEY:
-        features = Rec_Helper().get_features(value)
+        features = get_features(value)
         return(jsonify(features))
 
 
 @app.route('/recs/<key>/<value>')
 def recommendations(key, value):
     if key == API_KEY:
-        recs = Rec_Helper().rec_data(value)
+        recs = rec_data(value)
 
         return(jsonify(recs))
 
@@ -44,7 +44,7 @@ def recommendations(key, value):
 @app.route('/embed/<key>/<value>')
 def embed(key, value):
     if key == API_KEY:
-        recs = Rec_Helper().rec_data(value)
+        recs = rec_data(value)
 
         return(render_template('embed.html', recs=recs))
 
@@ -52,19 +52,18 @@ def embed(key, value):
 @app.route('/graph/<key>/<value>')
 def graph(key, value):
     if key == API_KEY:
-        rec_helper = Rec_Helper()
-        recs = rec_helper.top_recs(value)
+        recs = top_recs(value)
 
-        features = pd.concat([rec_helper.get_all_features([value]), recs])
+        features = pd.concat([get_all_features([value]), recs])
 
         radar_chart = pygal.Radar()
         radar_chart.title = ('Comparison of Recommendations for \"' +
-                             rec_helper.spotify.track(value)['name'] + '\"')
+                             spotify.track(value)['name'] + '\"')
         radar_chart.x_labels = list(features.keys())
 
         for id, feature_vec in zip(features.id, features.drop('id', axis=1).values):
-            radar_chart.add(rec_helper.spotify.track(id)['name'],
-                            rec_helper.spot_scaler.transform([feature_vec])[0])
+            radar_chart.add(spotify.track(id)['name'],
+                            spot_scaler.transform([feature_vec])[0])
 
         graph_data = radar_chart.render_data_uri()
 
@@ -75,20 +74,19 @@ def graph(key, value):
 @app.route('/graph_data/<key>/<value>')
 def graph_data(key, value):
     if key == API_KEY:
-        rec_helper = Rec_Helper()
-        recs = rec_helper.top_recs(value)
+        recs = top_recs(value)
 
-        features = pd.concat([rec_helper.get_all_features([value]), recs])
+        features = pd.concat([get_all_features([value]), recs])
         
-        feat_dict = rec_helper.songs_data(features)
+        feat_dict = songs_data(features)
         radar_chart = pygal.Radar()
         radar_chart.title = ('Comparison of Recommendations for \"' +
-                             rec_helper.spotify.track(value)['name'] + '\"')
+                             spotify.track(value)['name'] + '\"')
         radar_chart.x_labels = list(features.keys())
 
         for id, feature_vec in zip(features.id, features.drop('id', axis=1).values):
-            radar_chart.add(rec_helper.spotify.track(id)['name'],
-                            rec_helper.spot_scaler.transform([feature_vec])[0])
+            radar_chart.add(spotify.track(id)['name'],
+                            spot_scaler.transform([feature_vec])[0])
 
         graph_data = radar_chart.render_data_uri()
         graph_dict = [{'graph_uri':graph_data},feat_dict]
--- spotify_api/work_data.py
+++ spotify_api/work_data.py
@@ -10,120 +10,117 @@ from numpy.linalg import norm
 from joblib import load
 
 
-CLIENT_ID_var = config("CLIENT_ID")
-CLIENT_SECRET_var = config("CLIENT_SECRET")
-
-
-class Rec_Helper():
-    CLIENT_ID = CLIENT_ID_var
-    CLIENT_SECRET = CLIENT_SECRET_var
-    credentials = oauth2.SpotifyClientCredentials(
+CLIENT_ID = config("CLIENT_ID")
+CLIENT_SECRET = config("CLIENT_SECRET")
+credentials = oauth2.SpotifyClientCredentials(
                     client_id=CLIENT_ID,
                     client_secret=CLIENT_SECRET)
-    token = credentials.get_access_token()
+token = credentials.get_access_token()
 
-    def __init__(self, k=5):
-        self.spotify = spotipy.Spotify(auth=self.token)
-        self.spot_scaler = load('SpotScaler.joblib')
+spotify = spotipy.Spotify(auth=token)
+spot_scaler = load('SpotScaler.joblib')
 
-    # get the ids of 100 recommendations from spotify
-    def get_100(self, target_id):
-        res = self.spotify.recommendations(seed_tracks=[target_id], limit=100)
-        ids = [i['id'] for i in res['tracks']]
-        return(ids)
 
-    def get_features(self, target_id):
-        res = self.spotify.audio_features(tracks=target_id)[0]
+# get the ids of 100 recommendations from spotify
+def get_100(target_id):
+    res = spotify.recommendations(seed_tracks=[target_id], limit=100)
+    ids = [i['id'] for i in res['tracks']]
+    return(ids)
 
-        feat_dict = {'acousticness': res['acousticness'],
-                     'danceability': res['danceability'],
-                     'duration_ms': res['duration_ms'],
-                     'energy': res['energy'],
-                     'instrumentalness': res['instrumentalness'],
-                     'key': res['key'],
-                     'liveness': res['liveness'],
-                     'loudness': res['loudness'],
-                     'mode': res['mode'],
-                     'speechiness': res['speechiness'],
-                     'tempo': res['tempo'],
-                     'time_signature': res['time_signature'],
-                     'valence': res['valence'],
-                     'id': res['id']}
-        return feat_dict
 
-    def get_all_features(self, list_id):
-        res = self.spotify.audio_features(tracks=list_id)
+def audio_features_to_dict(feats):
+    feat_dict = {'acousticness': feats['acousticness'],
+                 'danceability': feats['danceability'],
+                 'duration_ms': feats['duration_ms'],
+                 'energy': feats['energy'],
+                 'instrumentalness': feats['instrumentalness'],
+                 'key': feats['key'],
+                 'liveness': feats['liveness'],
+                 'loudness': feats['loudness'],
+                 'mode': feats['mode'],
+                 'speechiness': feats['speechiness'],
+                 'tempo': feats['tempo'],
+                 'time_signature': feats['time_signature'],
+                 'valence': feats['valence'],
+                 'id': feats['id']}
+    return feat_dict
 
-        df = pd.DataFrame(columns=['acousticness',
-                                   'danceability',
-                                   'duration_ms',
-                                   'energy',
-                                   'instrumentalness',
-                                   'key',
-                                   'liveness',
-                                   'loudness',
-                                   'mode',
-                                   'speechiness',
-                                   'tempo',
-                                   'time_signature',
-                                   'valence',
-                                   'id'])
 
-        for i in res:
-            feat_dict = {'acousticness': i['acousticness'],
-                         'danceability': i['danceability'],
-                         'duration_ms': i['duration_ms'],
-                         'energy': i['energy'],
-                         'instrumentalness': i['instrumentalness'],
-                         'key': i['key'],
-                         'liveness': i['liveness'],
-                         'loudness': i['loudness'],
-                         'mode': i['mode'],
-                         'speechiness': i['speechiness'],
-                         'tempo': i['tempo'],
-                         'time_signature': i['time_signature'],
-                         'valence': i['valence'],
-                         'id': i['id']}
-            df = df.append(feat_dict, ignore_index=True)
-        return(df)
+def get_features(target_id):
+    res = spotify.audio_features(tracks=target_id)[0]
 
-    def top_recs(self, target_id, k=5):
-        df = self.get_all_features(self.get_100(target_id))
-        song_df = self.get_all_features(target_id)
+    return audio_features_to_dict(res)
 
-        df_scaled = self.spot_scaler.fit_transform(df.drop(['id'], axis=1))
-        feats = self.spot_scaler.transform(song_df.drop(['id'], axis=1))
 
-        df['dist'] = list(map(lambda x: norm(x-np.array(feats)[0]),
-                              np.array(df_scaled)))
-        top = df.sort_values(by='dist').iloc[0:k]
+def get_all_features(list_id):
+    feats_list = spotify.audio_features(tracks=list_id)
 
-        return top.drop(['dist'], axis=1)
+    df = pd.DataFrame(columns=['acousticness',
+                               'danceability',
+                               'duration_ms',
+                               'energy',
+                               'instrumentalness',
+                               'key',
+                               'liveness',
+                               'loudness',
+                               'mode',
+                               'speechiness',
+                               'tempo',
+                               'time_signature',
+                               'valence',
+                               'id'])
 
-    # Get an array of data pertaining to a list of recommendations
-    def songs_data(self, res_df):
-        vals = res_df.id.values
-        song_info = []
-        for i in vals:
-            tracks = self.spotify.track(i)
-            song_info.append({'large_image': tracks['album']['images'][0]['url'],
-                              'med_image': tracks['album']['images'][1]['url'],
-                              'small_image': tracks['album']['images'][2]['url'],
-                              'artist': tracks['artists'][0]['name'],
-                              'song_name': tracks['name'],
-                              'id': tracks['id'],
-                              'uri': tracks['uri']})
-        return song_info
+    for feat in feats_list:
+        feat_dict = audio_features_to_dict(feat)
+        df = df.append(feat_dict, ignore_index=True)
+    return(df)
 
-    # Get an array of data pertaining to a list of recommendations based on target
-    def rec_data(self, target_id):
-        return self.songs_data(self.top_recs(target_id))
 
-    def get_songs(self, song, limit=7):
-        songs = self.spotify.search(song, limit=limit, offset=0, type='track', market='US')
-        tracks = []
-        for i in songs['tracks']['items']:
-            tracks.append({'song_name': i['name'],
-                           'artist': i['artists'][0]['name'],
-                           'id': i['id'], 'uri':i['uri']})
-        return(tracks)
+def top_recs(target_id, k=5):
+    df = get_all_features(get_100(target_id))
+    song_df = get_all_features(target_id)
+
+    df_scaled = spot_scaler.fit_transform(df.drop(['id'], axis=1))
+    feats = spot_scaler.transform(song_df.drop(['id'], axis=1))
+
+    df['dist'] = list(map(lambda x: norm(x-np.array(feats)[0]),
+                          np.array(df_scaled)))
+    top = df.sort_values(by='dist').iloc[0:k]
+
+    return top.drop(['dist'], axis=1)
+
+
+# Get an array of data pertaining to a list of recommendations
+def songs_data(res_df):
+    vals = res_df.id.values
+    song_info = []
+    for i in vals:
+        tracks = spotify.track(i)
+        song_info.append({'large_image': tracks['album']['images'][0]['url'],
+                          'med_image': tracks['album']['images'][1]['url'],
+                          'small_image': tracks['album']['images'][2]['url'],
+                          'artist': tracks['artists'][0]['name'],
+                          'song_name': tracks['name'],
+                          'id': tracks['id'],
+                          'uri': tracks['uri']})
+    return song_info
+
+
+# Get an array of data pertaining to a list of recommendations based on target
+def rec_data(target_id):
+    return songs_data(top_recs(target_id))
+
+
+def get_songs(song, limit=7):
+    songs = spotify.search(song,
+                           limit=limit,
+                           offset=0,
+                           type='track',
+                           market='US')
+    tracks = []
+    for i in songs['tracks']['items']:
+        tracks.append({'song_name': i['name'],
+                       'artist': i['artists'][0]['name'],
+                       'id': i['id'],
+                       'uri': i['uri']})
+    return(tracks)
