09f49d141868ab7056b40f18abc4b65d95cb50fe
AHartNtkn <AHartNtkn@users.noreply.github.com>
2019-10-21 18:32:58 -0700

    Refactor get_data and Five_recs

--- spotify_api/app.py
+++ spotify_api/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, url_for, redirect,jsonify
-from get_data import *
 from decouple import config
 from dotenv import load_dotenv
 from work_data import *
+import pygal
+
 
 load_dotenv()
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def home():
@@ -15,35 +15,25 @@ def home():
 
 @app.route('/songs/<value>')
 def song_search(value):
-    songs = get_songs(value)
+    songs = Rec_Helper().get_songs(value)
     return(jsonify(songs))
 
 @app.route('/features/<value>')
 def song_features(value):
-    features = get_features(value)
+    features = Rec_Helper().get_features(value)
     return(jsonify(features))
 
 @app.route('/recs/<value>')
 def recommendations(value):
-    recs = Five_recs(value)
-    all_recs = recs.get_100()
-    feature_df = recs.get_all_features(all_recs)
-    target_song = recs.get_all_features(value)
-    five_recs_df = recs.top_recs(feature_df, target_song)
-    five_recs = recs.feedback(five_recs_df)
+    recs = Rec_Helper().feedback(value)
 
-    return(jsonify(five_recs))
-
+    return(jsonify(recs))
 
 @app.route('/embed/<value>')
 def embed(value):
-    recs = Five_recs(value)
-    all_recs = recs.get_100()
-    feature_df = recs.get_all_features(all_recs)
-    target_song = recs.get_all_features(value)
-    five_recs_df = recs.top_recs(feature_df, target_song)
-    five_recs = recs.feedback(five_recs_df)
-    return(render_template('embed.html', five_recs=five_recs))
+    recs = Rec_Helper().feedback(value)
+
+    return(render_template('embed.html', recs=recs))
 
 if __name__ == '__main__':
     app.run()
