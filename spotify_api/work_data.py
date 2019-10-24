941ba975f4bb51edd61d3cb6f87ea4e3ba4bdf47
AHartNtkn <AHartNtkn@users.noreply.github.com>
2019-10-23 20:56:37 -0700

    remove redundant dataframe construction

--- spotify_api/work_data.py
+++ spotify_api/work_data.py
@@ -123,26 +123,18 @@ def songs_data(res_df):
     vals = res_df.id.values
     song_info = []
     for i in vals:
-        tracks = spotify.track(i)
-        song_info.append({'large_image': tracks['album']['images'][0]['url'],
-                          'med_image': tracks['album']['images'][1]['url'],
-                          'small_image': tracks['album']['images'][2]['url'],
-                          'artist': tracks['artists'][0]['name'],
-                          'song_name': tracks['name'],
-                          'id': tracks['id'],
-                          'uri': tracks['uri']})
+        song_info.append(songs_data_single(i))
     return song_info
 
 def songs_data_single(id):
-
     tracks = spotify.track(id)
     t = {'large_image': tracks['album']['images'][0]['url'],
-                      'med_image': tracks['album']['images'][1]['url'],
-                      'small_image': tracks['album']['images'][2]['url'],
-                      'artist': tracks['artists'][0]['name'],
-                      'song_name': tracks['name'],
-                      'id': tracks['id'],
-                      'uri': tracks['uri']}
+         'med_image': tracks['album']['images'][1]['url'],
+         'small_image': tracks['album']['images'][2]['url'],
+         'artist': tracks['artists'][0]['name'],
+         'song_name': tracks['name'],
+         'id': tracks['id'],
+         'uri': tracks['uri']}
     return t
 
 # Get an array of data pertaining to a list of recommendations based on target
